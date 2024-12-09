5cf0e4b4d838ffda332c18b6694e7a451d482bba
trann-namm <namsherlock44@outlook.com>
2024-12-09 10:44:05 +0700

    delete .gitignore file

---
+++ .config.py
@@ -0,0 +1,11 @@
+mysql_host = "mysql"
+mysql_user = "root"
+mysql_password = "root"
+mysql_database = "startup_db"
+mysql_table_name = "startups"
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+}
